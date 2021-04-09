edab3ef73d3e26dc3be753b5debc04c2a2cdf779
Eric Hagen <eboy.coltt5@gmail.com>
2021-04-09 16:51:04 -0700

    Change how current pack is displayed

--- src/commands/public/current.ts
+++ src/commands/public/current.ts
@@ -11,15 +11,12 @@ export default class implements Command {
         const resultEmbed = new MessageEmbed()
             .setTitle(curPack.packName)
             .setColor(`#${(Math.random() * 0xFFFFFF + 1).toString(16)}`)
-            .setDescription(`[Download Mappack](${curPack.downloadUrl})`)
-            .addFields(curPack.maps.map(m => ({
-                name: `${m.artist} - ${m.title}`,
-                value: m.versions.reduce((p, c) =>
-                    `${p}\n${c.version} - ${c.value}`
-                , `[Info](https://osu.ppy.sh/beatmapsets/${m.setId}) | ` +
-                    `[Download](https://d/${m.setId})`),
-                inline: true
-            })))
+            .setDescription(curPack.maps.reduce((p, m) =>
+                `${p}\n[${m.artist} - ${m.title}](https://osu.ppy.sh/beatmapsets/${m.setId})${
+                    m.versions.sort((a, b) => a.value - b.value).reduce((pv, v) => `${pv} | ${v.value}`, "").slice(2)
+                }`
+            , ''))
+            .addField("\u200b", `[Download Mappack](${curPack.downloadUrl})`)
             .setTimestamp();
         return msg.channel.send(resultEmbed);
     };
